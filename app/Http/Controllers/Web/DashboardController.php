1d65176e84e25891d745743e555e71c01b456778
Ivan Martinez <igmrivan@gmail.com>
2024-07-08 11:17:30 -0600

    Update dashboard

--- app/Http/Controllers/Web/DashboardController.php
+++ app/Http/Controllers/Web/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Models\Stock;
 use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
@@ -10,8 +11,9 @@ class DashboardController extends Controller
     public function index()
     {
         $data = [
-            'menu'    => 'dashboard',
-            'topTeen' => $this->topTeen(),
+            'menu'         => 'dashboard',
+            'topTeen'      => $this->topTeen(),
+            'listStockMin' => $this->listStockMin(),
         ];
         return view('app.dashboard', compact('data'));
     }
@@ -30,5 +32,34 @@ class DashboardController extends Controller
             ORDER BY stock.id DESC
             LIMIT 10;');
         return $query;
+    }
+
+    public function listStockMin()
+    {
+        return DB::table('stock')
+            ->join('cartridges', 'cartridges.id', '=', 'stock.cartridge_id')
+            ->join('printers', 'printers.id', '=', 'cartridges.printer_id')
+            ->join('brands', 'brands.id', '=', 'printers.brand_id')
+            ->select([
+                'brands.description AS brand', 'printers.description AS printer',
+                'cartridges.model', 'cartridges.description AS cartridge', DB::raw('SUM(stock._quantity) AS quantity')
+            ])
+            ->groupBy(['brands.description', 'printers.description', 'cartridges.model', 'cartridges.description'])
+            ->havingRaw('SUM(stock._quantity) < ?', [2])
+            ->orderByDesc('stock.created_at')
+            ->paginate(5);
+
+        return DB::select(
+            'SELECT brands.description AS brand, printers.description AS printer,
+                cartridges.model, cartridges.description AS cartridge, SUM(stock._quantity) AS quantity
+            FROM stock
+                INNER JOIN cartridges ON stock.cartridge_id = cartridges.id
+                INNER JOIN printers ON cartridges.printer_id = printers.id
+                INNER JOIN brands ON printers.brand_id = brands.id
+            WHERE 1
+            GROUP BY brands.description, printers.description, cartridges.model, cartridges.description
+            HAVING SUM(stock._quantity) < 2
+            ORDER BY stock.created_at DESC;'
+        );
     }
 }
--- resources/views/app/dashboard.blade.php
+++ resources/views/app/dashboard.blade.php
@@ -1,6 +1,7 @@
 @php
     $menu = $data['menu'];
     $topTeen = $data['topTeen'];
+    $listStockMin = $data['listStockMin'];
 @endphp
 <x-master>
     @slot('menu', $menu)
@@ -33,30 +34,68 @@
                         </div>
                         <!-- /.card-header -->
                         <div class="card-body p-0">
-                            <ul class="products-list product-list-in-card pl-2 pr-2">
-                                @foreach ($topTeen as $item)
-                                    @php
-                                        $dateAt = date('Y-m-d', strtotime($item->created_at));
-                                        $color = 'danger';
-                                        if ($item->type == 1) {
-                                            $color = 'success';
-                                        }
-                                    @endphp
-                                    <li class="item">
-                                        <div class="product-info">
-                                            <a href="javascript:void(0)" class="product-title">{{ $item->cartridge }}
-                                                <span
-                                                    class="badge badge-{{$color}} float-right">{{ $item->_quantity }}</span></a>
-                                            <span class="product-description">
-                                                {{ $dateAt }}
-                                            </span>
-                                        </div>
-                                    </li>
-                                    <!-- /.item -->
-                                @endforeach
-                            </ul>
+                            <table class="table table-sm">
+                                <thead>
+                                    <tr>
+                                        <th>Cartridge</th>
+                                        <th style="width: 99px;">Date at</th>
+                                        <th style="width: 10px;">Qty</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($topTeen as $item)
+                                        @php
+                                            $dateAt = date('Y-m-d', strtotime($item->created_at));
+                                            $color = 'danger';
+                                            if ($item->type == 1) {
+                                                $color = 'success';
+                                            }
+                                        @endphp
+                                        <tr>
+                                            <td>{{ $item->cartridge }}</td>
+                                            <td>{{ $dateAt }}</td>
+                                            <td><span class="badge bg-{{ $color }}">{{ $item->_quantity }}</span>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
                         </div>
                         <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-header bg-navy">
+                            <h3 class="card-title"><i class="fas fa-exchange-alt mr-1"></i> Recently operations</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body p-0">
+                            <table class="table table-sm">
+                                <thead>
+                                    <tr>
+                                        <th>Model</th>
+                                        <th>Cartridge</th>
+                                        <th style="width: 10px;">Qty</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($listStockMin as $item)
+                                        <tr>
+                                            <td>{{ $item->model }}</td>
+                                            <td>{{ $item->cartridge }}</td>
+                                            <td>{{ $item->quantity }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer">
+                                {{ $listStockMin->links() }}
+                        </div>
+                        <!-- /.card-footer -->
                     </div>
                     <!-- /.card -->
                 </div>
